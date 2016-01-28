c024b1b6c8f1e89f04256f374a2e43294c2c9e0e
Mike Drogowski <mrdrogow@mtu.edu>
2016-01-27 21:38:58 -0500

    GameObject Documentation
    Added documentation to GameObject class for other team members.

--- src/ag1/GameObject.java
+++ src/ag1/GameObject.java
@@ -3,13 +3,25 @@ package ag1;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
+/**
+ * Superclass for all physical game objects. Includes parameters and methods for
+ * getting and setting position, velocity, dimensions, bounds, gravity. Also
+ * requires that subclasses include tick (for updating the objects properties)
+ * and render (for redrawing the object) methods.
+ * 
+ * @author Michael Drogowski (mrdrogow@mtu.edu)
+ */
 public abstract class GameObject {
 	
-	protected float x, y;
-	protected float velX = 0, velY = 0;
-	protected int width, height;
-	protected float gravity = 0.9f;
+	protected float x, y; //pixel coordinates for objects top-left corner
+	protected float velX = 0, velY = 0; //velocity of object (if movement added)
+	protected int width, height; //pixel width and height of object 
+	protected float gravity = 0.9f; //gravity applied to object
 	
+	/**
+	 * Empty constructor. For objects that position and size do not matter. Sets
+	 * x, y, width, and height to 0.
+	 */
 	public GameObject() {
 		this.x = 0;
 		this.y = 0;
@@ -17,6 +29,13 @@ public abstract class GameObject {
 		this.height = 0;
 	}
 	
+	/**
+	 * Constructor with x and y position arguments, but sets width and height to
+	 * 0. For objects where width and height do not matter.
+	 * 
+	 * @param x X-coordinate of objects top-left corner.
+	 * @param y Y-coordinate of objects top-left corner.
+	 */
 	public GameObject(float x, float y) {
 		this.x = x;
 		this.y = y;
@@ -24,6 +43,15 @@ public abstract class GameObject {
 		this.height = 0;
 	}
 	
+	/**
+	 * Constructor with x and y position arguments along with object width and
+	 * height.
+	 * 
+	 * @param x X-coordinate of objects top-left corner.
+	 * @param y Y-coordinate of objects top-left corner.
+	 * @param width Pixel width of object (hitbox - for collision).
+	 * @param height Pixel height of object (hitbox - for collision).
+	 */
 	public GameObject(float x, float y, int width, int height) {
 		this.x = x;
 		this.y = y;
@@ -32,7 +60,7 @@ public abstract class GameObject {
 	}
 	
 	public float getX(){
-		return x; //bottom left pixel x position
+		return x;
 	}
 	
 	public void setX(float x){
@@ -40,7 +68,7 @@ public abstract class GameObject {
 	}
 	
 	public float getY(){
-		return y; //bottom left pixel y position
+		return y;
 	}
 	
 	public void setY(float y){
@@ -79,11 +107,29 @@ public abstract class GameObject {
 		this.height = height;
 	}
 	
+	/**
+	 * Returns the object's bounds based on its x, y, width, and height. Used
+	 * for collision.
+	 * 
+	 * @return Rectangle of the object's bounds.
+	 */
 	public Rectangle getBounds(){
 		return new Rectangle((int)x, (int)y, width, height);
 	}
 	
+	/**
+	 * Put mechanics to update/check every game tick in here (Examples: x, y,
+	 * velocities, collision, etc). Will be called every game tick. Leave empty
+	 * if object is "static" (does not move/change).
+	 */
 	public abstract void tick();
+	
+	/**
+	 * Put visuals to be redrawn/rendered by Graphics2D in here. Will be called
+	 * every game tick. Leave empty if no visuals.
+	 * 
+	 * @param g The Graphics2D object used for rendering the object.
+	 */
 	public abstract void render(Graphics2D g);
 
 }
